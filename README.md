e2740df9ce06b0c912825d41591e4348d275fc97
hyoda <hdseo@basic9.co.kr>
2023-08-13 09:32:37 +0900

    initialized

---
+++ README.md
@@ -0,0 +1,10 @@
+# chatbot
+
+
+## 필요사항
+### 환경변수 셋팅
+.streamlit 파일에 OPEN_API_KEY를 셋팅하고 테스트 하시면 됩니다.
+### 패키지 인스톨
+```
+pip install -r requirements.txt
+```
---
+++ sida-chat-with-st-chat-message.py
@@ -0,0 +1,39 @@
+import openai
+import streamlit as st
+import numpy as np
+from streamlit_chat import message
+
+openai.api_key = st.secrets['pass']
+
+st.title("시다 챗: 남해워케이션에서 만든 인공지능 챗봇")
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
