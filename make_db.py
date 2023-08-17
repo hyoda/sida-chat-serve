bdec4a0f9c85cb89566d39b2cd9f00fc56117e9e
blackdew <blackdew7@gmail.com>
2023-08-17 09:05:27 +0900

    db 갱신

--- make_db.py
+++ make_db.py
@@ -16,20 +16,31 @@ def split_docs(documents, chunk_size=chunk_size, chunk_overlap=chunk_overlap):
     )
     return text_splitter.split_documents(documents)
 
-
-documents = DirectoryLoader(DATA_DIR, glob="./novel/*.txt", loader_cls=TextLoader).load()
+documents = DirectoryLoader(DATA_DIR, glob="카페/*.txt", loader_cls=TextLoader).load()
 doc = split_docs(documents)
 print(len(doc))
 
-documents = DirectoryLoader(DATA_DIR, glob="./달리기-런닝/*.txt", loader_cls=TextLoader).load()
+documents = DirectoryLoader(DATA_DIR, glob="음식점/*.txt", loader_cls=TextLoader).load()
 doc += split_docs(documents)
 print(len(doc))
 
-documents = DirectoryLoader(DATA_DIR, glob="./서핑/*.txt", loader_cls=TextLoader).load()
+documents = DirectoryLoader(DATA_DIR, glob="달리기-런닝/*.txt", loader_cls=TextLoader).load()
 doc += split_docs(documents)
 print(len(doc))
 
-documents = DirectoryLoader(DATA_DIR, glob="./기타/*.txt", loader_cls=TextLoader).load()
+documents = DirectoryLoader(DATA_DIR, glob="펜션/*.txt", loader_cls=TextLoader).load()
+doc += split_docs(documents)
+print(len(doc))
+
+documents = DirectoryLoader(DATA_DIR, glob="서핑/*.txt", loader_cls=TextLoader).load()
+doc += split_docs(documents)
+print(len(doc))
+
+documents = DirectoryLoader(DATA_DIR, glob="기타/*.txt", loader_cls=TextLoader).load()
+doc += split_docs(documents)
+print(len(doc))
+
+documents = DirectoryLoader(DATA_DIR, glob="블로그/*.txt", loader_cls=TextLoader).load()
 doc += split_docs(documents)
 print(len(doc))
 
